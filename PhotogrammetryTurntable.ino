a95f2f24c655cf8d2c35a1531190530e862b1511
Michael Stevenson <michael@mstevenson.net>
2022-07-07 07:53:20 -0700

    Cleanup

--- PhotogrammetryTurntable.ino
+++ PhotogrammetryTurntable.ino
@@ -2,16 +2,12 @@
 #include "DFRemote.h"
 
 #define IDLE 0
-#define CAPTURING 1
-#define IMPORTING 2
-#define MOVING 3
+#define CAPTURE 1
+#define IMPORT 2
+#define MOVE 3
 
-int state = IDLE;
+const int startButtonPin = 3;
 
-const int buttonPin = 3;
-const int ledPin = 13;
-
-// stepper pins
 const int dirPin = 4;
 const int stepPin = 5;
 const int sleepPin = 6;
@@ -19,7 +15,8 @@ const int sleepPin = 6;
 const int totalFrames = 30;
 const int stepsPerRevolution = 1600;
 
-int buttonState = 0;
+int state = IDLE;
+int dfMessage;
 int frame = 0;
 
 DFRemote df = DFRemote();
@@ -29,8 +26,7 @@ void setup()
 {
   Serial.begin(57600);
   
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(startButtonPin , INPUT);
 
   stepper.setEnablePin(sleepPin);
   stepper.setMaxSpeed(1000);
@@ -44,48 +40,51 @@ void loop()
   {
     case IDLE:
     {
-      digitalWrite(ledPin, LOW);
-      // Enable stepper motor sleep to reduce heat and power consumption while idle
       stepper.disableOutputs();
-      buttonState = digitalRead(buttonPin);
-      if (buttonState == HIGH)
+      if (digitalRead(startButtonPin) == HIGH)
       {
-        stepper.enableOutputs(); // disable stepper sleep
-        delay(500);
-        state = CAPTURING;
+        stepper.enableOutputs();
+        delay(200);
+        state = CAPTURE;
       }
       break;
     }
-    case CAPTURING:
+    case CAPTURE:
     {
       df.shootFrame(1);
-      state = IMPORTING;
+      frame++;
+      state = IMPORT;
       break;
     }
-    case IMPORTING:
+    case IMPORT:
     {
-      // Wait for DF to finish capturing the frame
-      int cmd = df.processSerial();
-      if (cmd != DF_CC_MSG)
+      int msg = df.processSerial();
+      if (msg != DF_CC_MSG)
       {
         return;
       }
-      state = MOVING;
-      break;
-    }
-    case MOVING:
-    {
       if (frame < totalFrames)
       {
-        frame++;
-        stepper.runToNewPosition(frame * (stepsPerRevolution/totalFrames));
-        state = CAPTURING;
+        stepper.moveTo(frame * (stepsPerRevolution/totalFrames));
+        state = MOVE;
       }
       else
       {
+        stepper.runToNewPosition(0); // blocking
         frame = 0;
-        stepper.runToNewPosition(0);
         state = IDLE;
+      }
+      break;
+    }
+    case MOVE:
+    {
+      if (stepper.distanceToGo() != 0)
+      {
+        stepper.run();
+      }
+      else
+      {
+        state = CAPTURE;
       }
       break;
     }
